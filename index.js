2f7c9c4a181c3b4aca10f2dc4c636e3002b4193c
Saurabh Trivedi <trivedi@qualcomm.com>
2019-03-15 13:13:44 -0700

    node-express final

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-const express = require('express'),
-     http = require('http');
-
+const express = require('express');
+const http = require('http');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log(req.headers);
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 
-});
+app.use(bodyParser.json());
+
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
 
 const server = http.createServer(app);
 
